9450b538cb6cdbdf0fc00af3a64ee9bb58a331ee
Layfully <adriangaborek3@gmail.com>
2023-04-09 15:39:00 +0200

    feat: add finished project

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # UdacityLandingPage
+
+This project is a simple landing page that demonstrates how to manipulate the Document Object Model (DOM) using JavaScript. It consists of several sections, each with content, and a navigation bar that allows users to quickly navigate to any section on the page.
+
+## Getting Started
+
+To get started with this project, simply clone the repository and open the `index.html` file in your web browser.
+
+
+## Features
+
+- Responsive design that looks great on all devices
+- Dynamic navigation bar that updates automatically as sections are added or removed
+- Smooth scrolling animation when clicking on navigation links
+- Simple and easy-to-read code that demonstrates fundamental DOM manipulation techniques
+- Scroll to top button
+- Hide navigation when idle
+- Show active section in the menu and in the website
+
+## License
+
+This project is licensed under the MIT License - see the `LICENSE` file for details.
+
+## Acknowledgments
+
+This project was inspired by the [Udacity Front End Developer Nanodegree](https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011).
--- js/app.js
+++ js/app.js
@@ -23,6 +23,13 @@
  * 
 */
 
+const sections = document.querySelectorAll('section');
+const header = document.querySelector('.page__header');
+const navbar = document.getElementById('navbar__list');
+const scrollToTopButton = document.getElementById('scrollToTopButton');
+const pageFold = window.innerHeight;
+let navigationAnchors = [];
+let headerHideTimeout;
 
 /**
  * End Global Variables
@@ -30,6 +37,28 @@
  * 
 */
 
+function hideHeader() {
+  header.classList.add('page__header--hidden');
+}
+
+function showHeader() {
+  header.classList.remove('page__header--hidden');
+}
+
+//returns the value of its href attribute, without the leading # character
+function getCleanHref(element) {
+  return element.getAttribute('href').slice(1);
+}
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
 
 /**
@@ -39,13 +68,86 @@
 */
 
 // build the nav
+function buildMenu() {
+  const navFragment = document.createDocumentFragment()
+
+  for (section of sections) {
+    const listItem = document.createElement('li');
+    const anchor = document.createElement('a');
+    navigationAnchors.push(anchor);
+
+    const data = section.getAttribute('data-nav');
+
+    anchor.setAttribute('href', `#${section.id}`);
+    anchor.setAttribute('data-nav', data);
+
+    anchor.textContent = data;
+    anchor.classList.add('menu__link');
+
+    anchor.addEventListener('click', scrollTo);
+
+    listItem.appendChild(anchor);
+    navFragment.appendChild(listItem);
+  }
+
+  navbar.appendChild(navFragment);
+}
+
+// Adds the class your-active-class to the currently active section
+// and the class navbar__menu--active to the corresponding navigation link.
+// It removes both classes from all other sections and navigation links
+function setActiveClass() {
+  for (section of sections) {
+    const rect = section.getBoundingClientRect();
+
+    if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+      const navItem = document.querySelector(`a[data-nav="${section.getAttribute('data-nav')}"]`);
+
+      for (anchor of navigationAnchors) {
+        anchor.classList.remove('navbar__menu--active');
+      }
+
+      navItem.classList.add('navbar__menu--active');
+      section.classList.add('your-active-class');
+
+      return;
+    }
+
+    section.classList.remove('your-active-class');
+  }
+}
+
+// Scroll to anchor ID using scrollTO event`
+function scrollTo(event) {
+  event.preventDefault();
+  document.getElementById(getCleanHref(event.target)).scrollIntoView({ behavior: "smooth" });
+}
+
+// Shows and hides the navigation menu depending on whether the user is scrolling or not.
+// It uses a timer to hide the menu after a certain amount of time.
+function controlVisibilityOfMenu() {
+  window.clearTimeout(headerHideTimeout);
+  headerHideTimeout = setTimeout(hideHeader, 1000);
+  showHeader();
+}
+
+// Shows and hides the scroll-to-top button depending on whether the user
+// has scrolled past the height of the viewport.
+function controlVisibilityOfToTopButton() {
+  if (window.pageYOffset > pageFold) {
+    scrollToTopButton.classList.add('show');
+  } else {
+    scrollToTopButton.classList.remove('show');
+  }
+}
 
 
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
 
 /**
  * End Main Functions
@@ -54,7 +156,17 @@
 */
 
 // Build menu 
-
 // Scroll to section on link click
+window.addEventListener('DOMContentLoaded', buildMenu)
 
 // Set sections as active
+window.addEventListener('scroll', setActiveClass);
+
+// Show/Hide menu when scrolling/idle
+window.addEventListener('scroll', controlVisibilityOfMenu);
+
+// Control visibility of scroll to top button
+window.addEventListener('scroll', controlVisibilityOfToTopButton);
+
+// Scroll to top on button click
+scrollToTopButton.addEventListener('click', scrollToTop);
